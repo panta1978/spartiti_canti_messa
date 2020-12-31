357cc25d3d33f47b9c7f3fcf6952c3db81582ebd
panta1978 <stefano.pantaleoni@gmail.com>
2020-12-31 14:01:19 +0100

    v4.0

--- Create_Book.py
+++ Create_Book.py
@@ -16,37 +16,22 @@ from docx import Document
 import comtypes.client
 start_time = time.time()
 
-
 # Folders and Files
 PdfFolder = os.getcwd() + '\\pdf'
 MusxFolder = os.getcwd() + '\\mus'
 IndexTemplate = os.getcwd() + '\\Index_Template.docx'
 OutputFile = os.getcwd() + '\\LibrettoCanti.pdf'
 
-
-# List of .pdf Files
-PdfFiles = []
-for root, dirs, files in os.walk(PdfFolder):
-    for filename in files:
-        if filename[-4:]=='.pdf':
-            PdfFiles.append(filename[:-4])
-            
-
-# List of .musx Files
-MusxFiles = []
-for root, dirs, files in os.walk(MusxFolder):
-    for filename in files:
-        if filename[-5:]=='.musx':
-            MusxFiles.append(filename[:-5])
-#MusxFiles = MusxFiles[1:] # Remove 1st entry (template)
-
+# List of .pdf and .musx Files
+PdfFiles = [x[:-4] for x in os.listdir(PdfFolder) if x.endswith('.pdf')]
+MusxFiles = [x[:-5] for x in os.listdir(MusxFolder) if x.endswith('.musx')]
 
 # Prepare sorted list of .pdf files (non-case sensitive alphabetical order with some characters removed or modified)
 PdfFiles_X = []; PdfFiles_X = PdfFiles[:]
 S_Find = np.array(['à', 'è', 'é', 'ì', 'ò', 'ù', '-', '\''])
 S_Repl = np.array(['a', 'e', 'e', 'i', 'o', 'u', ' ', ' ' ])
 for xfp in range(0, len(PdfFiles)):
-    PdfFiles_X[xfp] = PdfFiles[xfp].lower();
+    PdfFiles_X[xfp] = PdfFiles[xfp].lower()
     # Replace special characters with regular counterparts
     for xs in range(0, len(S_Find)):
         PdfFiles_X[xfp] = PdfFiles_X[xfp].replace(S_Find[xs], S_Repl[xs])
@@ -54,46 +39,25 @@ for xfp in range(0, len(PdfFiles)):
     while PdfFiles_X[xfp].find('  ')>=0:
         PdfFiles_X[xfp] = PdfFiles_X[xfp].replace('  ', ' ')
 NS = np.argsort(PdfFiles_X)
-    
 
 # Sort .pdf files
-PdfFiles_S = []
-PdfFiles_XS = []
-for xn in range(0,len(NS)):
-    PdfFiles_S.append(PdfFiles[NS[xn]])
-    PdfFiles_XS.append(PdfFiles_X[NS[xn]])
+PdfFiles_S = [PdfFiles[n] for n in NS]
+PdfFiles_XS = [PdfFiles_X[n] for n in NS]
 
-    
 # Look for EXCEEDING .pdf Files (.musx counterpart not present)
-print(' ')
-print('====================')        
-print(' ')
+print('\n' + '='*32 + '\n')
 print('EXCEEDING PDF FILES:')
-for xfp in range(0, len(PdfFiles_S)):
-    FilePresent = 0
-    for xfm in range(0, len(MusxFiles)):
-        if PdfFiles_S[xfp]==MusxFiles[xfm]:
-            FilePresent = 1
-    if FilePresent==0:
-        print('* ' + PdfFiles_S[xfp])
-print(' ')
-print('====================')        
-print(' ')
-
+for PdfFile in PdfFiles_S:
+    if not(PdfFile in MusxFiles):
+        print('* ' + PdfFile)
+print('\n' + '='*32 + '\n')
 
 # Look for MISSING .pdf Files (only .musx file present)
 print('MISSING PDF FILES:')
-for xfm in range(0, len(MusxFiles)):
-    FilePresent = 0
-    for xfp in range(0, len(PdfFiles)):
-        if MusxFiles[xfm]==PdfFiles[xfp]:
-            FilePresent = 1
-    if FilePresent==0:
-        print('* ' + MusxFiles[xfm])
-print(' ')
-print('====================')        
-print(' ')
-
+for MusxFile in MusxFiles:
+    if not(MusxFile in PdfFiles):
+        print('* ' + MusxFile)
+print('\n' + '='*32 + '\n')
 
 # Create Index string
 IdxStr = ''
@@ -101,13 +65,11 @@ for xfp in range(0, len(PdfFiles_S)):
     IdxStr = IdxStr + PdfFiles_S[xfp] + '\n'
 IdxStr = IdxStr[:-1]
 
-
 # Create .docx temporary file with updated Song Index
 document = Document(IndexTemplate)
 paragraphs = document.paragraphs
 paragraphs[-1].text = IdxStr
 document.save(os.getcwd() + '\\Index_Temp.docx')
-
 
 # Export .docx to .pdf
 wdFormatPDF = 17
@@ -117,11 +79,9 @@ doc.SaveAs(os.getcwd() + '\\Index_Temp.pdf', FileFormat=wdFormatPDF)
 doc.Close()
 word.Quit()
 
-
 # Delete temporary .docx file
 if os.path.exists(os.getcwd() + '\\Index_Temp.docx'):
     os.remove(os.getcwd() + '\\Index_Temp.docx')
-
 
 # Import Index.pdf
 A=PdfFileReader(os.getcwd() + '\\Index_Temp.pdf')
@@ -131,7 +91,6 @@ if Np/2!=np.floor(Np/2):
     AddBlankPageAfterIndex = 1
 else:
     AddBlankPageAfterIndex = 0
-    
 
 # Calculate starting pages
 NPageStart = []; NPageStart.append(Np+1)  # 1st file (except for index.pdf) starting page
@@ -148,7 +107,6 @@ for xfp in range(0, len(PdfFiles_S)):
         AddBlankPage.append(0)
 AddBlankPage.append(0)
 
-
 # Output .pdf file. Add index
 output = PdfFileWriter()
 f = os.getcwd() + '\\Index_Temp.pdf'
@@ -159,7 +117,6 @@ output.addBookmark('INDICE', 2)
 if AddBlankPageAfterIndex==1:
     output.addBlankPage()
 
-    
 # Output .pdf file. Add other files    
 for xfp in range(0, len(PdfFiles_S)):
     if AddBlankPage[xfp]==1:
@@ -169,7 +126,6 @@ for xfp in range(0, len(PdfFiles_S)):
     for p in range(pdfFile.getNumPages()):
         output.addPage(pdfFile.getPage(p))
 
-
 # Add Bookmarks
 CurrInit = '-'
 for xfp in range(0,len(PdfFiles_XS)):
@@ -177,7 +133,6 @@ for xfp in range(0,len(PdfFiles_XS)):
         CurrInit = PdfFiles_XS[xfp][0]
         parent = output.addBookmark(CurrInit.upper(), NPageStart[xfp]-1+AddBlankPage[xfp])
     output.addBookmark(PdfFiles_S[xfp], NPageStart[xfp]-1+AddBlankPage[xfp], parent)
-
 
 # Collapse Bookmarks
 output.addJS('function closeBookmarks(bm)' + \
@@ -188,17 +143,14 @@ output.addJS('function closeBookmarks(bm)' + \
              'closeBookmarks(this.bookmarkRoot);' \
              )
 
-
 # Create output .pdf file
 with open(OutputFile, 'wb') as outputStream:
     output.write(outputStream)
 
-
 # Delete temporary Index.pdf file
 if os.path.exists(os.getcwd() + '\\Index_Temp.pdf'):
     os.remove(os.getcwd() + '\\Index_Temp.pdf')
-    
-    
+
 # Speed report
 stop_time = time.time()
 DT = stop_time-start_time
